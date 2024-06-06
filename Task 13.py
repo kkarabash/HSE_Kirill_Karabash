7da984f44b19b7adb05d19f3f7f0988f6c86c95d
Kirill Karabash <150274189+kkarabash@users.noreply.github.com>
2024-06-06 20:48:54 +0300

    upd

---
+++ Task 13.py
@@ -0,0 +1,6 @@
+def is_palindrome(number: int) -> bool:
+    # Преобразуем число в строку
+    num_str = str(number)
+    # Проверяем, равно ли строковое представление числа его обратному
+    return num_str == num_str[::-1]
+print(is_palindrome(127))
